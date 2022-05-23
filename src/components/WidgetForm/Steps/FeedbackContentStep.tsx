b0e1ec0ff4f131f00f4c1026faa90c756945b367
egfs1 <eduardogomesfs4@gmail.com>
2022-05-23 18:10:16 -0300

    loading while sending feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
-import { ArrowLeft, Camera } from "phosphor-react"
+import { ArrowLeft} from "phosphor-react"
 import { FormEvent, useState } from "react"
 import { FeedbackType, feedbackTypes } from ".."
+import { api } from "../../../services/api"
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../../Loading"
 import { ScreenshotButton } from "../ScreenshotButton"
 
 interface IFeedbackContentStep {
@@ -13,14 +15,22 @@ interface IFeedbackContentStep {
 export function FeedbackContentStep(props: IFeedbackContentStep){
     const [screenshot, setScreenshot] = useState<string | null>(null)
     const [comment, setComment] = useState('')
+    const [isSendingFeedback, setIsSendingFeedback] = useState(false)
+
     const feedbackTypeInfo = feedbackTypes[props.feedbackType]
 
-    function handleSumbitFeedback(event: FormEvent){
+    async function handleSumbitFeedback(event: FormEvent){
         event.preventDefault()
-        console.log({
-            screenshot,
-            comment
+
+        setIsSendingFeedback(true)
+        
+        await api.post('/feedbacks', {
+            type: props.feedbackType,
+            comment,
+            screenshot
         })
+
+        setIsSendingFeedback(false)
 
         props.onFeedbackSent()
     }
@@ -49,10 +59,10 @@ export function FeedbackContentStep(props: IFeedbackContentStep){
                     <ScreenshotButton screenshot={screenshot} onScreenshotTook={setScreenshot}/>
                     <button
                         type="submit"
-                        disabled={comment.length===0 || screenshot===null}
+                        disabled={comment.length===0 || screenshot===null || isSendingFeedback}
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
-                        Enviar Feedback
+                        {isSendingFeedback ? <Loading/> : 'Enviar Feedback'}
                     </button>
                 </footer>
 
