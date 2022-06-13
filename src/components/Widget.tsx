4ef275ef1ea3a3f0face84fe17c512bf2d1fab2a
egfs1 <eduardogomesfs4@gmail.com>
2022-06-13 19:05:45 -0300

    backend disabled message

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -4,18 +4,23 @@ import { WidgetForm } from "./WidgetForm"
 
 export function Widget(){
     return (
-        <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
-            <Popover.Panel>
-                <WidgetForm />
-            </Popover.Panel>
+            <div className="flex justify-center items-center min-h-screen min-w-screen">
+                <p className="font-mono text-md sm:text-lg md:text-2xl animate-type overflow-x-hidden group-hover:animate-type-reverse whitespace-nowrap" style={{willChange: 'transform'}}>{"<p> Back-end disabled :( </p>"}</p>
+                <span className="box-border inline-block w-1.5 md:w-2 h-7 md:h-9 ml-2 bg-white animate-pulse"></span>
+                
+                <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+                    <Popover.Panel>
+                        <WidgetForm />
+                    </Popover.Panel>
 
-            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
-                <ChatTeardropDots className="w-6 h-6"/>
+                    <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+                        <ChatTeardropDots className="w-6 h-6"/>
 
-                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-                    <span className="pl-2">Feedback</span>
-                </span>
-            </Popover.Button>
-        </Popover>
+                        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                            <span className="pl-2">Feedback</span>
+                        </span>
+                    </Popover.Button>
+                </Popover>
+            </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,46 @@ module.exports = {
         borderRadius: {
           md: '4px'
         }
-      }
+      },
+      animation: {
+        type: 'type 1s ease-out .8s 1 normal both',
+      },
+      keyframes: {
+        type: {
+          '0%': { width: '0ch' },
+          '3.44%': { width: '1ch' },
+          '6.88%': { width: '2ch' },
+          '10.32%': { width: '3ch' },
+          '13.76%': { width: '4ch' },
+          '17.2%': { width: '5ch' },
+          '20.64%': { width: '6ch' },
+          '24.08%': { width: '7ch' },
+          '27.52%': { width: '8ch' },
+          '30.96%': { width: '9ch' },
+          '33.44%': { width: '10ch' },
+          '37.84%': { width: '11ch' },
+          '41.28%': { width: '12ch' },
+          '44.72%': { width: '13ch' },
+          '48.16%': { width: '14ch' },
+          '51.6%': { width: '15ch' },
+          '55.04%': { width: '16ch' },
+          '58.48%': { width: '17ch' },
+          '61.92%': { width: '18ch' },
+          '65.36%': { width: '19ch' },
+          '68.8%': { width: '20ch' },
+          '72.24%': { width: '21ch' },
+          '75.68%': { width: '22ch' },
+          '79.12%': { width: '23ch' },
+          '82.56%': { width: '24ch' },
+          '86%': { width: '25ch' },
+          '89.44%': { width: '26ch' },
+          '92.88%': { width: '27ch' },
+          '96.32%': { width: '28ch' }
+        },
+        pulse: {
+          '50%': {opacity: 0}
+        }
+      },
     },
   },
   plugins: [
